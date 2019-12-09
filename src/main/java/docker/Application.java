5794ecc8b9f9378fc849c51e926c69b4cd41ae00
Lukasz <lukasz.dudek97@gmail.com>
2019-12-09 19:30:59 +0100

    Tests

--- src/main/java/docker/Application.java
+++ src/main/java/docker/Application.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
-    private String test;
-
     @RequestMapping("/")
     public String home() {
         return "Hello Docker World";
