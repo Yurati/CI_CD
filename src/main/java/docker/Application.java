158fe7140bb34163800a37963efa8c7dc4db38b8
Lukasz <lukasz.dudek97@gmail.com>
2019-12-15 21:12:04 +0100

    Jenkinsfile update

--- src/main/java/docker/Application.java
+++ src/main/java/docker/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "IO_2019";
     }
 
     public static void main(String[] args) {
