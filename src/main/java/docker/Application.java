1e4a2f34cf7460966fdfde933f5ce3e3361d12ee
Lukasz <lukasz.dudek97@gmail.com>
2019-12-15 22:39:45 +0100

    Jenkinsfile update

--- src/main/java/docker/Application.java
+++ src/main/java/docker/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "IO_CICD_2019";
+        return "IO_CICD_2019!";
     }
 
     public static void main(String[] args) {
